7f7f4c3b93762cb265ddf568ad80df8c3a5f5412
nnurallar-commits <nnurallar@gmail.com>
2026-08-19 15:17:01 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,19 +10,48 @@ const FILES_TO_CACHE = [
     "./images/icon-512.png"
 ];
 
+// Uygulama kurulurken dosyaları cache'e al
 self.addEventListener("install", event => {
+    self.skipWaiting();
+
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(FILES_TO_CACHE))
     );
 });
 
+// Eski cache'leri temizle
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+// İnternet varsa güncel dosyayı al,
+// olmazsa cache'deki sürümü kullan
 self.addEventListener("fetch", event => {
     event.respondWith(
         fetch(event.request)
             .then(response => {
+                const responseClone = response.clone();
+
+                caches.open(CACHE_NAME)
+                    .then(cache => {
+                        cache.put(event.request, responseClone);
+                    });
+
                 return response;
             })
-            .catch(() => caches.match(event.request))
+            .catch(() => {
+                return caches.match(event.request);
+            })
     );
 });
