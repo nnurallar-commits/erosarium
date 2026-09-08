9d4524259f1f5daa68251bdd5d8e5a5989381c0c
nnurallar-commits <nnurallar@gmail.com>
2026-09-08 15:53:05 +0300

    Add files via upload

---
+++ images/service-worker.js
@@ -0,0 +1,190 @@
+const CACHE_NAME =
+    "erosarium-v45-logo";
+
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./style-v5.css?v=8",
+    "./images/erosarium-icon-192-v8.png",
+    "./images/erosarium-icon-512-v8.png",
+    "./images/erosarium-icon-180-v8.png",
+    "./script.js?v=5",
+    "./manifest.json?v=8"
+];
+
+
+self.addEventListener(
+    "install",
+    event => {
+
+        self.skipWaiting();
+
+        event.waitUntil(
+            caches
+                .open(
+                    CACHE_NAME
+                )
+                .then(
+                    cache =>
+                        cache.addAll(
+                            APP_FILES
+                        )
+                )
+        );
+
+    }
+);
+
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        event.waitUntil(
+
+            Promise.all([
+
+                caches
+                    .keys()
+                    .then(keys => {
+
+                        return Promise.all(
+
+                            keys.map(key => {
+
+                                if (
+                                    key.startsWith("erosarium-") && key !==
+                                    CACHE_NAME
+                                ) {
+
+                                    return caches
+                                        .delete(
+                                            key
+                                        );
+
+                                }
+
+                            })
+
+                        );
+
+                    }),
+
+                self.clients.claim()
+
+            ])
+
+        );
+
+    }
+);
+
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        const request =
+            event.request;
+
+
+        if (
+            request.method !==
+            "GET"
+        ) {
+
+            return;
+
+        }
+
+
+        const url =
+            new URL(
+                request.url
+            );
+
+
+        if (
+            url.origin !==
+            self.location.origin
+        ) {
+
+            return;
+
+        }
+
+
+        event.respondWith(
+
+            fetch(request)
+
+                .then(response => {
+
+                    if (
+                        response &&
+                        response.status ===
+                        200
+                    ) {
+
+                        const copy =
+                            response.clone();
+
+
+                        caches
+                            .open(
+                                CACHE_NAME
+                            )
+                            .then(
+                                cache => {
+
+                                    cache.put(
+                                        request,
+                                        copy
+                                    );
+
+                                }
+                            );
+
+                    }
+
+
+                    return response;
+
+                })
+
+                .catch(
+                    async () => {
+
+                        const cached =
+                            await caches
+                                .match(
+                                    request
+                                );
+
+
+                        if (cached) {
+
+                            return cached;
+
+                        }
+
+
+                        if (
+                            request.mode ===
+                            "navigate"
+                        ) {
+
+                            return caches
+                                .match(
+                                    "./index.html"
+                                );
+
+                        }
+
+                    }
+                )
+
+        );
+
+    }
+);
